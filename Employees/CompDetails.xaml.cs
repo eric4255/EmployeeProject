fb165a722a329efaf61b7e0254264390e5e28c8d
Olga Osinskaya <ssinsk@yandex.ru>
2017-11-17 21:51:01 -0800

    First commit of Employee app

---
+++ Employees/CompDetails.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Employees
+{
+    /// <summary>
+    /// Interaction logic for CompDetails.xaml
+    /// </summary>
+    public partial class CompDetails : Page
+    {
+        public CompDetails()
+        {
+            InitializeComponent();
+        }
+
+        public CompDetails(Object data) : this()
+        {
+            this.DataContext = data;
+
+            if (data is Employee)
+            {
+                Employee emp = (Employee)data;
+                string name = "";
+                string value= "";
+
+                emp.GetSpareProp1(ref name, ref value);
+                this.SpareProp1Name.Content = name;
+                this.SpareProp1Value.Content = value;
+                SpareProp1.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
